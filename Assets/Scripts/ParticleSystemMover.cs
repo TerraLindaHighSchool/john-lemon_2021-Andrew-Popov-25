5fb25f8eb6ed134131044406cbd49b4e0aef5498
AndrewBruhMoment <89489268+Andrew-Popov-25@users.noreply.github.com>
2022-01-05 23:01:04 -0800

    Added Particle Effects

---
+++ Assets/Scripts/ParticleSystemMover.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParticleSystemMover : MonoBehaviour
+{
+    public GameObject JohnLemon;
+
+    void Update()
+    {
+        transform.position = Vector3.MoveTowards(transform.position, JohnLemon.transform.position, Time.deltaTime);
+    }
+}
